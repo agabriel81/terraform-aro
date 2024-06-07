138c636ae379e2f0c8dc60c6d0bb0ea05a79e4de
root <root@aro-neu-cluster-jumphost.pmkul3wx5syu3jhxtyo24sedaa.fx.internal.cloudapp.net>
2024-06-07 15:20:00 +0000

    changes App GW conf from Ansible

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -191,7 +191,7 @@ resource "azurerm_application_gateway" "aro_appgw" {
 
   backend_address_pool {
     name = local.backend_address_pool_name
-    ip_addresses = [local.aro_default_ingress_ip]
+//    ip_addresses = [local.aro_default_ingress_ip]
   }
 
   backend_http_settings {
